9e8e55db72ab6dec28033d6238c4c54b8bc97282
AparnaBharti12 <129382327+AparnaBharti12@users.noreply.github.com>
2023-12-28 00:31:42 +0530

    Update and rename BankingZeroBalanceAccount.py to Project_preview.py

--- Assignments/PYTHON/BankingZeroBalanceAccount.py
+++
@@ -1,9 +0,0 @@
-from DatabaseConnection import *
-from BankingAccount import Account
-
-
-class ZeroBalanceAccount(Account):
-
-    def __init__(self, account_id, customer_id, account_type):
-        self.connection = create_connection()
-        super().__init__(account_id, customer_id, account_type, 0)
