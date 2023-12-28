05ef81e3fc963ff33b844d6138ee4140c413a3aa
AparnaBharti12 <129382327+AparnaBharti12@users.noreply.github.com>
2023-12-28 22:29:36 +0530

    Delete Assignments/PYTHON/Project_preview.py

--- Assignments/PYTHON/Project_preview.py
+++
@@ -1,7 +0,0 @@
-"""
-                          BANKING SYSTEM
-    You are tasked with creating an advanced banking system that includes various types of accounts, such 
-as savings and current accounts. The system should support account creation, deposits, withdrawals, and 
-interest calculations.
-
-"""
